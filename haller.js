2f67076be6dbbf8964603b7f2459c4085d968bc6
Sercan Eraslan <info@sercaneraslan.com>
2012-12-31 01:04:16 +0200

    Update haller.js

--- haller.js
+++ haller.js
@@ -7,22 +7,22 @@
 (function(){
 
     function Haller(isim, hal){
-        this.isim                       = isim.charAt(0).toUpperCase() + isim.slice(1);
-        this.hal                        = hal;
-        this.sesliHarfler               = ["a", "ı", "o", "u", "e", "i", "ö", "ü"];
-        this.sertUnsuzler               = ["f", "s", "t", "k", "ç", "ş", "h", "p"];
-        this.sapkaliHarfIcerenIsimler   = ["Alp", "Şevval", "Kemal", "Bilal", "Nihal", "Hilal", "Meral", "Resul", "Celal", "Cemal"];
-        this.harf                       = this.sesliHarfler.indexOf(this.isim.substr(this.isim.length-1,1));
-        this.harf2                      = this.sesliHarfler.indexOf(this.isim.substr(this.isim.length-2,1));
-        this.harf3                      = this.sesliHarfler.indexOf(this.isim.substr(this.isim.length-3,1));
-        this.sonuSertUnsuz              = this.sertUnsuzler.indexOf(this.isim.substr(this.isim.length-1,1));
-        this.sapkaliIsimler             = this.sapkaliHarfIcerenIsimler.indexOf(this.isim);
-        this.ekinSonHarfi;
-        this.ekler;
-        this.ek;
+        this.isim = isim.charAt(0).toUpperCase() + isim.slice(1);
+        this.hal = hal;
     }
 
     Haller.prototype = {
+        sesliHarfler                : ["a", "ı", "o", "u", "e", "i", "ö", "ü"],
+        sertUnsuzler                : ["f", "s", "t", "k", "ç", "ş", "h", "p"],
+        sapkaliHarfIcerenIsimler    : ["Alp", "Şevval", "Kemal", "Bilal", "Nihal", "Hilal", "Meral", "Resul", "Celal", "Cemal"],
+        ekinSonHarfi                : '',
+        ekler                       : '',
+        ek                          : '',
+        harf                        : '',
+
+        isminSonSesliHarfi: function(sayi){
+            return this.sesliHarfler.indexOf(this.isim.substr(this.isim.length-sayi,1));
+        },
         eklereKararVer: function(){
             if(this.hal == "e"){
                 this.ekler = ["ya", "ye", "a", "e"];
@@ -42,6 +42,8 @@
             return this.ekler;
         },
         ekiOlustur: function(a,b,c,d,e,f){
+            this.harf = this.isminSonSesliHarfi(1);
+
             if(this.hal == "i" || this.hal == "iyelik"){
                 if(this.harf == 0 || this.harf == 1){
                     this.ek = this.ekler[a];
@@ -60,23 +62,23 @@
             }
             return this.ek;
         },
-        ekeKararVer : function(){
+        ekeKararVer: function(){
             this.ekiOlustur(0,1,2,3,0,1);
 
             if(this.harf == -1){
-                (this.harf2 == -1) ? this.harf = this.harf3 : this.harf = this.harf2;
+                (this.harf2 == -1) ? this.harf = this.isminSonSesliHarfi(3) : this.harf = this.isminSonSesliHarfi(2);
                 this.ekiOlustur(4,5,6,7,2,3);
             }
             return this.ek;
         },
         sapkaliHarfVarMi: function(){
-            if(this.sapkaliIsimler !== -1){
+            if(this.sapkaliHarfIcerenIsimler.indexOf(this.isim) !== -1){
                 (this.hal == "i" || this.hal == "iyelik") ? this.ek = this.ekler[6] : this.ek = this.ekler[3];
             }
             return this.ek;
         },
         sonuSertUnsuzMu: function(){
-            if(this.sonuSertUnsuz !== -1){
+            if(this.sertUnsuzler.indexOf(this.isim.substr(this.isim.length-1,1)) !== -1){
                 if(this.hal == "de" || this.hal == "den"){
                     ekinSonHarfi = this.ek.slice(1, 3);
                     this.ek = "t" + ekinSonHarfi;
