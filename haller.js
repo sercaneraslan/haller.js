7592dabe89f7daeb8d79aa55b5f313738d46c16b
Sercan Eraslan <info@sercaneraslan.com>
2012-12-31 01:42:50 +0200

    Update haller.js

--- haller.js
+++ haller.js
@@ -15,10 +15,6 @@
         sesliHarfler                : ["a", "ı", "o", "u", "e", "i", "ö", "ü"],
         sertUnsuzler                : ["f", "s", "t", "k", "ç", "ş", "h", "p"],
         sapkaliHarfIcerenIsimler    : ["Alp", "Şevval", "Kemal", "Bilal", "Nihal", "Hilal", "Meral", "Resul", "Celal", "Cemal"],
-        ekinSonHarfi                : '',
-        ekler                       : '',
-        ek                          : '',
-        harf                        : '',
 
         isminSonSesliHarfi: function(sayi){
             return this.sesliHarfler.indexOf(this.isim.substr(this.isim.length-sayi,1));
@@ -63,7 +59,7 @@
         ekeKararVer: function(){
             this.harf = this.isminSonSesliHarfi(1);
             this.ekiOlustur(0,1,2,3,0,1);
-            
+
             if(this.harf == -1){
                 (this.isminSonSesliHarfi(2) == -1) ? this.harf = this.isminSonSesliHarfi(3) : this.harf = this.isminSonSesliHarfi(2);
                 this.ekiOlustur(4,5,6,7,2,3);
@@ -79,8 +75,8 @@
         sonuSertUnsuzMu: function(){
             if(this.sertUnsuzler.indexOf(this.isim.substr(this.isim.length-1,1)) !== -1){
                 if(this.hal == "de" || this.hal == "den"){
-                    ekinSonHarfi = this.ek.slice(1, 3);
-                    this.ek = "t" + ekinSonHarfi;
+                    this.ekinSonHarfi = this.ek.slice(1, 3);
+                    this.ek = "t" + this.ekinSonHarfi;
                 }
             }
             return this.ek;
