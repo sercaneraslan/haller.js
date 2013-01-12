03ff4c2ea9e3eb9a804ceaa4d69229bdeae5318f
Sercan Eraslan <info@sercaneraslan.com>
2013-01-13 00:49:22 +0200

    Yeniden yazildi :)

--- haller.js
+++ haller.js
@@ -1,93 +1,79 @@
 /*
 *
-* Haller.js v1.0
+* Haller.js v2.0
 * sercaneraslan.com
 *
+* Kullanımı örnekleri:
+* console.log(Hal("sercan","de"));      > sercan'da
+* console.log(Hal("murat","iyelik"));   > murat'ın
+* console.log(Hal("osman","i"));        > osman'ı
+* console.log(Hal("alp","de"));         > alp'te
+* console.log(Hal("cemal","e"));        > cemal'e
 */
-var Haller = {};
-
-Haller.init = function(isim, hal){
-    var isim = isim.charAt(0).toUpperCase() + isim.slice(1),
-        sesliHarfler = ["a", "ı", "o", "u", "e", "i", "ö", "ü"],
-        sertUnsuzler = ["f", "s", "t", "k", "ç", "ş", "h", "p"],
-        sapkaliHarfIsimleri = ["Alp", "Bilal", "Nihal", "Hilal", "Meral", "Seval", "Şevval", "Kemal", "Resul", "Celal", "Cemal"],
-        harf = sesliHarfler.indexOf(isim.substr(isim.length-1,1)),
-        harf2 = sesliHarfler.indexOf(isim.substr(isim.length-2,1)),
-        harf3 = sesliHarfler.indexOf(isim.substr(isim.length-3,1)),
-        sonuSertUnsuz = sertUnsuzler.indexOf(isim.substr(isim.length-1,1)),
-        sapkaliIsimler = sapkaliHarfIsimleri.indexOf(isim),
-        ekinSonHarfi,
-        ekler,
+var Hal = function(isim, hal, undefined) {
+    var sesliHarfler = 'aıeiouöü',
+        sertUnsuzler = 'fstkçşhp',
+        iyelik = 'iyelik',
+        iHali = 'i',
+        eHali = 'e',
+        deHali = 'de',
+        denHali = 'den',
+        ekKontrolleri = {0 : 'ı', 1 : 'ı', 2 : 'i', 3 : 'i', 4 : 'u', 5 : 'u', 6 : 'ü', 7 : 'ü'},
+        sonSesliIndex,
+        sesliler=[],
+        oncekiSesli,
+        sonSesli,
+        sonHarf,
+        istisna,
+        code,
         ek,
+        i;
 
-        eklereKararVer = function(){
-            if(hal == "e"){
-                ekler = ["ya", "ye", "a", "e"];
-            }
-            else if(hal == "de"){
-                ekler = ["da", "de", "da", "de"];
-            }
-            else if(hal == "den"){
-                ekler = ["dan", "den", "dan", "den"];
-            }
-            else if(hal == "i"){
-                ekler = ["yı", "yu", "yi", "yü", "ı", "u", "i", "ü"];
-            }
-            else if(hal == "iyelik"){
-                ekler = ["nın", "nun", "nin", "nün", "ın", "un", "in", "ün"];
-            }
-            return ekler;
-        },
-        ekiOlustur = function(a,b,c,d,e,f){
-            if(hal == "i" || hal == "iyelik"){
-                if(harf == 0 || harf == 1){
-                    ek = ekler[a];
-                }
-                else if(harf == 2 || harf == 3){
-                    ek = ekler[b];
-                }
-                else if(harf == 4 || harf == 5){
-                    ek = ekler[c];
-                }
-                else if(harf == 6 || harf == 7){
-                    ek = ekler[d];
-                }
-            }else{
-                (harf <= 3) ? ek = ekler[e] : ek = ekler[f];
-            }
-            return ek;
-        },
-        ekeKararVer = function(){
-            ekiOlustur(0,1,2,3,0,1);
+    for (i in isim) {    // Ismin her bir harfi icin...
+        // Eger bu harf sesli ise sesliler listesine harf sira numarasini ekle,
+        // bu harfin isimdeki sira numarasini sonSesliIndex degiskenine ata (integer'a cevir)
+        (code = sesliHarfler.indexOf(isim[i])) > -1 && (sesliler.push(code) & (sonSesliIndex = ~~i));
+        // Bu harfi sonHarf degiskenine al
+        sonHarf = isim[i];
+    }
+    
+    sonSesli = sesliler.pop();
 
-            if(harf == -1){
-                (harf2 == -1) ? harf = harf3 : harf = harf2;
-                ekiOlustur(4,5,6,7,2,3);
+    // Sapkali harf istisnasi var mı? Orn: Alp, Resul, Cemal...
+    if ( sonSesli == 0 || sonSesli == 5) {    // Son sesli harf a veya u ise
+        // son sesliden sonraki harf l ise
+        if (isim[sonSesliIndex + 1] == 'l') {
+            // onceki sesli e veya i ise ya da isim tek hece ise
+            oncekiSesli = sesliler.pop();
+            if ( oncekiSesli == undefined || oncekiSesli == 2 || oncekiSesli == 3) {
+                istisna = 1;
             }
-            return ek;
-        },
-        sapkaliHarfVarMi = function(){
-            if(sapkaliIsimler !== -1){
-                (hal == "i" || hal == "iyelik") ? ek = ekler[6] : ek = ekler[3];
-            }
-            return ek;
-        },
-        sonuSertUnsuzMu = function(){
-            if(sonuSertUnsuz !== -1){
-                if(hal == "de" || hal == "den"){
-                    ekinSonHarfi = ek.slice(1, 3);
-                    ek = "t" + ekinSonHarfi;
-                }
-            }
-            return ek;
-        };
+        }
+    }
 
-    eklereKararVer();
-    ekeKararVer();
-    sapkaliHarfVarMi();
-    sonuSertUnsuzMu();
+    ek = (hal == iyelik || hal == iHali) ?    // iyelik veya i hali ise
+            // sonSesli degiskeninin degeriyle ekKontrolleri objesinin n inci elemanına karsilik gelen obje degerini alır
+            ekKontrolleri[istisna ? sonSesli+2 : sonSesli]
+       : // e, de ve den hali ise
+            // Son sesli harf a, ı, o veya u ise ek a (istisna var ise e ), e, i, ö veya ü ise ek e harfi
+            ((sonSesli <= 1 || sonSesli == 4 || sonSesli == 5) ? istisna ? 'e' : 'a' : 'e');
+
+    // Kaynaştırma harflerini ekler
+    if ( sesliHarfler.indexOf( sonHarf ) > -1 ) {    // Son harf sesli harf ise
+        // hal iyelik ise ek in basina n harfi ekler, hal i ve e hali ise ek in basina y harfi ekler
+        ek = (hal == iyelik) ? 'n' + ek : (hal == iHali || hal == eHali) ? 'y' + ek : ek;
+    }
+
+    // Harf yumusamalarini kontrol eder
+    if (hal == deHali || hal == denHali) {    // de veya den hali ise
+        // Son harf sert ünsüz ise ek in basina t harfi ekler degilse d harfi ekler
+        ek = (sertUnsuzler.indexOf( sonHarf ) > -1 ? 't' : 'd') + ek;
+    }
+
+    // iyelik veya den hali icin ek in sonuna n harfi ekler
+    if (hal == iyelik || hal == denHali) {
+        ek += 'n' 
+    }
 
     return isim + "'" + ek;
-}
-
-console.log(Haller.init("sercan","de"));
+};
